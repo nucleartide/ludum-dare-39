f4dbee5f9369671a8be19ab45a5226c4041262f7
Jason Tu <jasontu4@gmail.com>
2017-07-28 20:58:36 -0400

    add timelapse script attribution

--- timelapse.sh
+++ timelapse.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# Thanks to http://ludumdare.com/compo/2010/08/20/osx-timelapse/
+
 while true ; do sleep 60 && echo `date`‘ Capturing screenshot…’ && screencapture -C -m -t jpg -x -f cap.`date +%s`.jpg ; done
